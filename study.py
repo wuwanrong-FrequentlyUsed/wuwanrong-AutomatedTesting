8baa705c03babaeaadc37f6c84964adbd64b2752
wuwanrong <wrwu719@163.com>
2025-05-06 17:54:18 +0800

    study

---
+++ study.py
@@ -0,0 +1,22 @@
+# python面向对象
+
+# 打印字符串
+print("hello world")
+
+print(5+3)
+
+print("hell world" * 8)
+
+print("hell world\n" * 8)
+
+# 会报错，str，int，类型不同
+# print("hell world" + 8)
+
+#5+8
+print(5+8)
+
+# 在一个字符串中嵌入一个双引号，正确的做法是:
+print(' i l"o"ve xxx ')
+
+# 计算一年有多少秒:
+print(365*24*60*60)
